f9d79920ebb14178140199414e2169357fd2d713
suryanshupalcsaiml23-hue <suryanshu.pal_cs.aiml23@gla.ac.in>
2026-02-02 09:56:37 +0530

    fix: force pg to use render DATABASE_URL only

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
-import pg from "pg";
-const { Pool } = pg;
-
-if (!process.env.DATABASE_URL) {
-  console.error("❌ DATABASE_URL is NOT set");
-  process.exit(1);
-}
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
